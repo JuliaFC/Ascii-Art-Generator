a00743a237ef8e58335ea818f50a47712fcabdcf
JuliaFC <mjfc@cin.ufpe.br>
2019-07-15 11:58:07 -0300

    Prune webpack_config.js

--- webpack_config.js
+++ webpack_config.js
@@ -5,20 +5,5 @@ module.exports = {
    output: {
      filename: 'bundle.js',
      path: path.resolve(__dirname, 'dist')
-   },
-   module: {
-     rules: [
-       {
-         test: /\.(js|jsx)$/,
-         exclude: /node_modules/,
-         use: {
-           loader: "babel-loader"
-         }
-       }
-     ]
-   },
-   stats: {
-       colors: true
-   },
-   devtool: 'source-map'
+   }
 };
